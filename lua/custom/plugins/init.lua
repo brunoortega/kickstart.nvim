73e5bae1fc962da1000a5b19358122ecc910c1ab
brunoortega <brunoortega11@gmail.com>
2024-09-01 03:41:04 -0300

    atualizado papi

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,12 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
+local lspconfig = require 'lspconfig'
+lspconfig.clangd.setup {
+  cmd = { 'clangd', '--background-index', '--clang-tidy', '--log=verbose' },
+  init_options = {
+    fallback_flags = { '-std=c++17' },
+  },
+}
 -- See the kickstart.nvim README for more information
 return {}
